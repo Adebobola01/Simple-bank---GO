867430d6a5406818c6cd59aa04b736eb70cb8249
Adebobola Oyedunmade <m.oyedunmade@gmail.com>
2025-09-06 05:11:00 +0100

    feat: added config file

--- main.go
+++ main.go
@@ -6,24 +6,24 @@ import (
 
 	"github.com/Adebobola01/Simple-bank---GO/api"
 	db "github.com/Adebobola01/Simple-bank---GO/db/sqlc"
+	"github.com/Adebobola01/Simple-bank---GO/util"
 	_ "github.com/lib/pq"
 )
 
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
 
 func main(){
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil{
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DbDriver, config.DbSource)
 	if err != nil{
 		log.Fatal("Cannot connect to db:", err)
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil{
 		log.Fatal("Cannot start server: ", err)
 	}
